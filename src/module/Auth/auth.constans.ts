5231d4ef9b16acf3007fbda9a5c10cc619a162ec
linux44gh6 <abireshan32@gmail.com>
2025-05-28 23:46:13 +0600

    add data

--- src/module/Auth/auth.constans.ts
+++ src/module/Auth/auth.constans.ts
@@ -4,7 +4,7 @@ import { IUser } from "../User/user.interface";
 
 export const createToken = (payload: Partial<IUser>) => {
     return jwt.sign(payload, config.jwt_secret as string, {
-        expiresIn:config.jwt_expires_in,
+        expiresIn:3,
     });
 };
 export const createRefreshToken = (payload: Partial<IUser>) => {
